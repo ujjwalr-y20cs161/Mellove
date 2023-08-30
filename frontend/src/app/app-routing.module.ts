03028c265e9229deef00431bcf47af7ff28c77dd
Ujjwal <y20cs161@rvrjc.ac.in>
2023-08-30 06:42:02 +0530

    Added Routers to component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthentComponent } from './authent/authent.component';
+import { FallbackComponent } from './fallback/fallback.component';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
+import { PlayerComponent } from './player/player.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'search', component: SearchComponent }, 
+  { path: 'player', component: PlayerComponent },
+  { path: 'authent', component: AuthentComponent },
+  { path: '**', component: FallbackComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/player/player.component.ts
+++ frontend/src/app/player/player.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent {
-
+  audio: HTMLAudioElement = new Audio();
+  constructor() { 
+    this.audio.src = "s3://music-bucket-2122/[iSongs.info] 04 - Raakhee Raakhee.mp3";
+  }
 }
